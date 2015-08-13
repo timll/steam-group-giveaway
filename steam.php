e1ba0507ff9e143ff633c6a38a6184c113bebfc8
Tim Lange <p@tim-lange.org>
2015-08-13 12:28:16 +0200

    Create steam.php

---
+++ steam.php
@@ -0,0 +1,19 @@
+<?php
+
+$groupname = $_GET['group'];
+
+$url = "http://steamcommunity.com/groups/".$groupname."/memberslistxml/?xml=1";
+$xml = simplexml_load_file($url);
+
+$members = array();
+for($i = 0; $i < $xml->memberCount; $i++) {
+    array_push($members, $xml->members->steamID64[$i]);
+}
+
+$winner = $members[rand(0, $xml->memberCount-1)];
+
+$url = "http://api.steampowered.com/ISteamUser/GetPlayerSummaries/v0002/?key=F71721765741C3262E75515B76955F21&steamids=". $winner ."&format=xml";
+$xml = simplexml_load_file($url);
+
+
+echo $winner . "TarU1LJqoNIR" .$xml->players->player->personaname . "EG9hLoUjP4xj" . $xml->players->player->avatarfull;
